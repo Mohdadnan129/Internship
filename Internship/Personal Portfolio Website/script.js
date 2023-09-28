e0e41f8ec63beb38b0a9bedcabf75df8d7aaa689
Mohdadnan129 <145915233+Mohdadnan129@users.noreply.github.com>
2023-09-28 13:23:46 +0530

    Add files via upload

---
+++ Internship/Personal Portfolio Website/script.js
@@ -0,0 +1,14 @@
+// Add smooth scrolling to navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const target = document.getElementById(targetId);
+
+        window.scroll({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
